2e0d2b10cbc802619bf6e821ce0f74e349fdc6d1
Fincher Justin <zhtsu47@me.com>
2016-06-04 23:58:19 +0800

    home page

--- WebTemplate/js/tab1.app.js
+++ WebTemplate/js/tab1.app.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
-    var mySwiper = new Swiper ('.swiper-container', {
+    var mySwiper = new Swiper ('.swiper-container',
+        {
             // Optional parameters
             direction: 'horizontal',
             loop: true,
             autoplay:400,
             // If we need pagination
-            pagination: '.swiper-pagination'
+            pagination: '.swiper-pagination',
+            autoplayDisableOnInteraction:false,
+            autoHeight:true
         })
 });
