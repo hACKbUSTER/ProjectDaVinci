798d28cbaa9ea664162f5d518f73386b67068f23
Fincher Justin <zhtsu47@me.com>
2016-06-04 21:04:16 +0800

    add Tabbar, banner pic

---
+++ WebTemplate/js/tab1.app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    var mySwiper = new Swiper ('.swiper-container', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+            autoplay:400,
+            // If we need pagination
+            pagination: '.swiper-pagination'
+        })
+});
